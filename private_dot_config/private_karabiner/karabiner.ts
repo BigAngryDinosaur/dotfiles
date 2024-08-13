3ecdf76e4fa6a33731fedbdfb0d8d8d891c51e95
rusha <hrishikesh.sawant322@gmail.com>
2024-08-13 22:20:45 +0900

    Add Keybard Maestro settings

--- private_dot_config/private_karabiner/karabiner.ts
+++ private_dot_config/private_karabiner/karabiner.ts
@@ -4,7 +4,5 @@ const args = Bun.argv;
 const isDryRun = args[2] === "--dry-run";
 
 writeToProfile(isDryRun ? "--dry-run" : "Default", [
-	rule("Demo").manipulators([map("c").to("v"), map("v").to("c")]),
-
 	rule("Caps Lock -> Hyper").manipulators([map("caps_lock").toHyper()]),
 ]);
