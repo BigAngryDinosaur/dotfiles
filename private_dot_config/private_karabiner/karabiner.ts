93880b25e02a91c29953c280374f03babd1ad55b
rusha <hrishikesh.sawant322@gmail.com>
2024-08-15 16:20:44 +0900

    modify kando menu

--- private_dot_config/private_karabiner/karabiner.ts
+++ private_dot_config/private_karabiner/karabiner.ts
@@ -1,8 +1,28 @@
-import { map, rule, writeToProfile } from "karabiner.ts";
+import {
+	ifVar,
+	map,
+	mapPointingButton,
+	rule,
+	toKey,
+	toPointingButton,
+	writeToProfile,
+} from "karabiner.ts";
 
 const args = Bun.argv;
 const isDryRun = args[2] === "--dry-run";
 
 writeToProfile(isDryRun ? "--dry-run" : "Default", [
+	// Hyper Key (Caps Lock)
 	rule("Caps Lock -> Hyper").manipulators([map("caps_lock").toHyper()]),
+
+	// Kando
+	rule("Kando").manipulators([
+		mapPointingButton("button2")
+			.toIfAlone("spacebar", "Hyper")
+			.toIfHeldDown(toPointingButton("button2")),
+
+		mapPointingButton("button4")
+			.toIfAlone(toKey("delete_forward"))
+			.toIfHeldDown(toPointingButton("button4")),
+	]),
 ]);
