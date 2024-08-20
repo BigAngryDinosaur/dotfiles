908f1c39b2edfe1d3f87df713bb4cd895651f3dc
rusha <hrishikesh.sawant322@gmail.com>
2024-08-20 11:43:43 +0900

    add general kando menu to button4 held down

--- private_dot_config/private_karabiner/karabiner.ts
+++ private_dot_config/private_karabiner/karabiner.ts
@@ -1,28 +1,27 @@
 import {
-	ifVar,
-	map,
-	mapPointingButton,
-	rule,
-	toKey,
-	toPointingButton,
-	writeToProfile,
+  map,
+  mapPointingButton,
+  rule,
+  toPointingButton,
+  writeToProfile,
 } from "karabiner.ts";
 
 const args = Bun.argv;
 const isDryRun = args[2] === "--dry-run";
 
 writeToProfile(isDryRun ? "--dry-run" : "Default", [
-	// Hyper Key (Caps Lock)
-	rule("Caps Lock -> Hyper").manipulators([map("caps_lock").toHyper()]),
+  // Hyper Key (Caps Lock)
+  rule("Caps Lock -> Hyper").manipulators([map("caps_lock").toHyper()]),
 
-	// Kando
-	rule("Kando").manipulators([
-		mapPointingButton("button2")
-			.toIfAlone("spacebar", "Hyper")
-			.toIfHeldDown(toPointingButton("button2")),
+  // Kando
+  rule("Kando").manipulators([
+    mapPointingButton("button4")
+      .toIfAlone(toPointingButton("button4"))
+      .toIfHeldDown({
+        key_code: "spacebar",
+        modifiers: ["left_shift", "left_command", "left_option", "left_control"],
+        halt: true
+      }),
+  ]),
 
-		mapPointingButton("button4")
-			.toIfAlone(toKey("delete_forward"))
-			.toIfHeldDown(toPointingButton("button4")),
-	]),
 ]);
