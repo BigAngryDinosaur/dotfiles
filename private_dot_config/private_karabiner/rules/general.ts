efe44153e59f175e4cb753f983add128e20bcf27
rusha <hrishikesh.sawant322@gmail.com>
2025-01-30 13:42:26 +0900

    add yabai rules

--- private_dot_config/private_karabiner/rules/general.ts
+++ private_dot_config/private_karabiner/rules/general.ts
@@ -1,8 +1,14 @@
 import { hyperLayer, map, rule, to$, toApp, toKey, withModifier } from "karabiner.ts";
 
+// Caps: 
+// tap: home
+// tap and hold: page_up
+// double tap: end
+// double tap and hold: meh
 
 export const generalMappings = () => [
   rule('Hyper Meh').manipulators([
+    map('home').toHyper(),
     map('page_up').toHyper(),
     map('page_down').toMeh(),
     {
