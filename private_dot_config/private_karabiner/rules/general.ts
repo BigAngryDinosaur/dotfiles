ad9e327af195be900cd88076f026f7d1c223257b
rusha <hrishikesh.sawant322@gmail.com>
2025-02-15 02:48:25 +0900

    Add shortcuts for websites

--- private_dot_config/private_karabiner/rules/general.ts
+++ private_dot_config/private_karabiner/rules/general.ts
@@ -17,15 +17,20 @@ export const generalMappings = () => [
     {
       'end': toKey('q', '⌘⌥'),
       'keypad_equal_sign': toKey('[', '⌘⌥'),
-      'home': toKey('return_or_enter', '⌘⌥'),
+      'home': toKey('return_or_enter', '⌘⌥')
     },
+    withModifier('Hyper')({
+      'w': toKey('f14'),
+      ',': toKey('f15'),
+      '.': toKey('f19'),
+    }),
     withModifier('right_shift')({
       ';': to$('open -g raycast://confetti')
     }),
   ]),
   doubleTap({ key_code: "right_shift" }, { key_code: "backslash", modifiers: ["left_option"] }, "Double Tap right shift to opt+backslash"),
   doubleTap({ key_code: "left_shift" }, { key_code: "keypad_4" }, "Double Tap left shift to keypad4"),
-  doubleTap({ key_code: "left_command" }, { key_code: "keypad_5" }, "Double Tap left shift to keypad5")
+  doubleTap({ key_code: "left_command" }, { key_code: "keypad_5" }, "Double Tap left shift to keypad5"),
 ]
 
 
