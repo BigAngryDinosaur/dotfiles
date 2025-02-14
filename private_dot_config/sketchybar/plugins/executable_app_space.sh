da3488e35bb63ebb60239eb6d19a0ce4aaefb72d
rusha <hrishikesh.sawant322@gmail.com>
2025-02-15 03:57:44 +0900

    fix sc

--- private_dot_config/sketchybar/plugins/executable_app_space.sh
+++ private_dot_config/sketchybar/plugins/executable_app_space.sh
@@ -17,7 +17,7 @@ then
    APPS=$(echo $QUERY | jq '.[].app')
    TITLES=$(echo $QUERY | jq '.[].title')
 
-   IGNORED_APPS=("Ghostty" "Reader" "Obsidian" "Finder" "ChatGPT" "Superwhisper" "Readwise Reader")
+   IGNORED_APPS=("Ghostty" "Reader" "Obsidian" "Finder" "ChatGPT" "Superwhisper" "Readwise Reader" "YouTube" "Hacker News" "Reddit" "Google AI Studio" "Claude" "DeepSeek")
  
    if grep -q "\"" <<< $APPS;
    then
