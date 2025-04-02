68c994ed7c007e1b54960d17b6c2158285bf1294
rusha <hrishikesh.sawant322@gmail.com>
2025-04-02 16:18:56 +0900

    add text navigation shortcuts

--- private_dot_config/private_karabiner/karabiner.ts
+++ private_dot_config/private_karabiner/karabiner.ts
@@ -4,15 +4,9 @@ import {
   writeToProfile
 } from "karabiner.ts";
 
-import layer_emojiAndSnippet from "./rules/emojis.ts";
-import layer_launchApp from "./rules/app_launcher.ts";
 import app_xcode from "./rules/xcode.ts";
 import app_vscode from "./rules/vscode.ts";
-import app_zed from "./rules/zed.ts";
-import app_cursor from "./rules/cursor.ts";
-import superwhisper from "./rules/superwhisper.ts";
 import { generalMappings } from "./rules/general.ts";
-import layer_openLinks from "./rules/links.ts";
 import app_chrome from "./rules/chrome.ts";
 import kando from "./rules/kando.ts";
 
@@ -24,18 +18,9 @@ writeToProfile(isDryRun ? "--dry-run" : "Default", [
 
   kando(),
 
-  layer_launchApp(),
-  layer_emojiAndSnippet(),
-  layer_openLinks(),
-
-
   app_xcode(),
   app_vscode(),
-  app_zed(),
-  app_cursor(),
   app_chrome(),
-
-  ...superwhisper()
 ]);
 
 
--- private_dot_config/private_karabiner/rules/general.ts
+++ private_dot_config/private_karabiner/rules/general.ts
@@ -1,32 +1,18 @@
-import { map, rule, to$, toKey, withModifier } from "karabiner.ts";
+import { map, mapConsumerKey, rule, to$, toKey, withModifier } from "karabiner.ts";
 import doubleTap from "./doubleTap";
 
-// | Caps            | Key Combination     |
-// |-----------------|---------------------|
-// | Single tap      | home                |
-// | Tap and hold    | page_up             |
-// | Double tap      | end                 |
-// | Double tap hold | meh                 |
-//
 // Modifiers: '⌘' | '⌥' | '⌃' | '⇧'
 
 export const generalMappings = () => [
   rule('General').manipulators([
-    map('page_up').toHyper(),
-    map('page_down').toMeh(),
-    {
-      'end': toKey('q', '⌘⌥'),
-      'keypad_equal_sign': toKey('[', '⌘⌥'),
-      'home': toKey('return_or_enter', '⌘⌥')
-    },
-    withModifier('Hyper')({
-      'w': toKey('f14'),
-      ',': toKey('f15'),
-      '.': toKey('f19'),
-    }),
-    withModifier('right_shift')({
-      ';': to$('open -g raycast://confetti')
-    }),
+    map('f13').to('a', '⌃'),
+    map('f14').to('left_arrow', '⌥'),
+    map('f14', '⌥').to('left_arrow', '⌥⇧'),
+    map('f14', '⌘').to('delete_or_backspace', '⌥'),
+    map('f15').to('right_arrow', '⌥'),
+    map('f15', '⌥').to('right_arrow', '⌥⇧'),
+    map('f15', '⌘').to('delete_forward'),
+    map('f16').to('e', '⌃'),
   ]),
   doubleTap({ key_code: "right_shift" }, { key_code: "backslash", modifiers: ["left_option"] }, "Double Tap right shift to opt+backslash"),
   doubleTap({ key_code: "left_shift" }, { key_code: "keypad_4" }, "Double Tap left shift to keypad4"),
